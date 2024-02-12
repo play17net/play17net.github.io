bdc9e823bc23ea2a37e0ef3aa0e283e495131ea0
play17net <lookcms@gmail.com>
2024-02-12 13:34:35 +0800

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,37 @@
+var timeoutId = 0;
+var redirectUrl = "universezy/dist/index.html";
+
+function checkParams () {
+    var url = window.location.href;
+    return checkRedirect(url, '?blog$')
+        || checkRedirect(url, '?category$')
+        || checkRedirect(url, '?column$');
+}
+
+function checkRedirect (url, flag) {
+    var index = url.indexOf(flag)
+    if (index > 0) {
+        var params = url.substring(index);
+        redirectUrl += params;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+redirect = () => {
+    window.location.href=redirectUrl;
+}
+
+window.onload = () => {
+    if (checkParams()) {
+        redirect();
+    } else {
+        timeoutId = setTimeout(redirect, 3000);
+    }
+}
+
+window.onclick = () => {
+    clearTimeout(timeoutId);
+    redirect();
+}
